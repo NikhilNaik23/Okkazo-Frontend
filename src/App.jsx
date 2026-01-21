12ddc5fe7c68d74b47f2c29f6ca8d36276d14d80
ChanduSugguna01 <chandusugguna@gmail.com>
2026-01-21 16:06:30 +0530

    add event details page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AdminLayout from "./components/Layout/admin/AdminLayout";
 import UserDashboard from "./pages/user/Dashboard";
 import PlanningWizard from "./pages/user/PlanningWizard";
 import PromoteEvent from "./pages/user/PromoteEvent";
+import AdminLayout from "./components/Layout/admin/AdminLayout";
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
       <Route path="/user/dashboard" element={<UserDashboard />} />
       <Route path="/user/planning-wizard" element={<PlanningWizard />} />
       <Route path="/user/promote" element={<PromoteEvent />} />
+      <Route path="/admin/*" element={<AdminLayout />} />
     </Routes>
     
   );
